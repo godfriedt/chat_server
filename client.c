ce53085c27443388174bf82bc5538a1484581c14
godfriedt <crunchz699@gmail.com>
2025-10-16 11:09:41 -0500

    server

---
+++ client.c
@@ -0,0 +1,14 @@
+#include <netinet/in.h>
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <stdio.h>
+#include <string.h>
+#include <pthread.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+#include <stdlib.h>
+
+int main(int argc, char* argv[]) {
+    return 0;
+}
